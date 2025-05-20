cdddd8dac52f50062867fb1ddaa9e305b1c99678
Bryan Guerra Cobos <bryan_9660@hotmail.com>
2025-05-20 16:01:32 +0200

    fix: update settings.py core for aws s3 objects parameters

--- core/settings.py
+++ core/settings.py
@@ -159,3 +159,9 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+    'ACL': 'public-read',
+    'ContentDisposition': 'inline'
+}
