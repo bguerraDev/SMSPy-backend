c1953e61bc2eaf6f2f26c34007b317d859c10c00
Bryan Guerra Cobos <bryan_9660@hotmail.com>
2025-05-21 15:43:30 +0200

    feat: views.py and settings.py for storage avatars on s3 bucket. Add storages_backends.py file. V11

--- user_messages/storages_backends.py
+++ user_messages/storages_backends.py
@@ -2,9 +2,9 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
-    default_acl = 'public-read'
+    location = 'media'
     file_overwrite = False
-    custom_domain = False
+    default_acl = None
 
     def get_object_parameters(self, name):
         return {
