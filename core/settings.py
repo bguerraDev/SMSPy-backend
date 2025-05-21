355fa9addaed95b84c8019c04d6047d1d65e0bd8
Bryan Guerra Cobos <bryan_9660@hotmail.com>
2025-05-21 17:03:30 +0200

    feat: views.py and settings.py for storage avatars on s3 bucket. Add storages_backends.py file. V19

--- core/settings.py
+++ core/settings.py
@@ -156,6 +156,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.eu-central-1.amazonaws.com
 
 # Media files
 #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'user_messages.storages_backends.MediaStorage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
