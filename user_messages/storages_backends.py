a12414e248f31bd143d69d81125eee0120aa6ffc
Bryan Guerra Cobos <bryan_9660@hotmail.com>
2025-05-21 16:06:16 +0200

    feat: views.py and settings.py for storage avatars on s3 bucket. Add storages_backends.py file. V15

--- user_messages/storages_backends.py
+++ user_messages/storages_backends.py
@@ -4,3 +4,15 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaStorage(S3Boto3Storage):
     file_overwrite = False
     default_acl = None
+
+    def get_object_parameters(self, name):
+        return {
+            "ContentType": self._guess_content_type(name),
+            "ContentDisposition": "inline",
+            "CacheControl": "max-age=86400",
+        }
+
+    def _guess_content_type(self, name):
+        import mimetypes
+        content_type, _ = mimetypes.guess_type(name)
+        return content_type or "application/octet-stream"
